cb4b6b7f1f31465eb32d097a1de0907cbed0a3de
tectoshi <116927616+tectoshi@users.noreply.github.com>
2023-12-18 15:54:16 +0900

    comparison-logical-operatorsの課題

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,27 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("こんにちは私はGeek君です。");
-		System.out.println(10);
-		System.out.println("20" + "24");
-		System.out.println(3.14);
-		System.out.println(true);
+		// aがbより大きい場合にtrue
+		int a = 13;
+		int b = 6;
+		boolean aIsBig = (a > b);
+		System.out.println(aIsBig);
+		
+		// 天気が晴れかつ暖かい場合にtrue
+		boolean isSunny = true;
+		boolean isWarm = true;
+		boolean isSunnyAndWarmResult = (isSunny && isWarm);
+		System.out.println(isSunnyAndWarmResult);
+		
+		// xが0以上かつyが偶数の場合にコンソールにtrue
+		int x = 3;
+		int y = 90;
+		boolean isResult3 = (x >= 0 && y % 2 ==0);
+		System.out.println(isResult3);
+		
+		// hasPermissionがfalseの場合にtrue
+		boolean hasPermission = false;
+		boolean isResult4 = !(hasPermission);
+		System.out.println(isResult4);
 	}
 }
