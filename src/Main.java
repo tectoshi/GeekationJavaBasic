661aa7438b814da474a5bce43cfbfaf561608e2b
tectoshi <116927616+tectoshi@users.noreply.github.com>
2023-12-18 13:12:17 +0900

    variablesの課題

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Geekation");
+	    int num1 = 17;
+	    double num2 = 1.4142;
+	    String myFavorite = "私の好きな";
+	    String desu = "です。";
+	    String dispNum1 = "{" + num1 + "}";
+	    String dispNum2 = "{" + num2 + "}";
+	    
+	    System.out.println(dispNum1);
+	    System.out.println(dispNum2);
+	    System.out.println(myFavorite + "整数は" + dispNum1 + desu);
+	    System.out.println(myFavorite + "小数は" + dispNum2 + desu);
 	}
 }
