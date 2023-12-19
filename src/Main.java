1d5685aed89f5d2e6a6c8f8244e1f7b9595ad1e7
tectoshi <116927616+tectoshi@users.noreply.github.com>
2023-12-19 09:57:02 +0900

    if-switch課題

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,41 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("こんにちは私はGeek君です。");
-		System.out.println(10);
-		System.out.println("20" + "24");
-		System.out.println(3.14);
-		System.out.println(true);
+		int num = 9;
+		if (num % 2 == 0 && num % 3 == 0) {
+			// 2と3の倍数
+			System.out.println("2の倍数です。");
+			System.out.println("3の倍数です。");
+		} else if (num % 2 == 0) {
+			// 2の倍数
+			System.out.println("2の倍数です。");
+		} else if (num % 3 == 0) {
+			// 3の倍数
+			System.out.println("3の倍数です。");
+		} else {
+			// どちらでもない
+			System.out.println("どちらでもありません。");
+		}
+		
+		String pref = "大阪";
+		switch (pref) {
+			case "北海":
+				// prefが"北海"の場合
+				System.out.println("北海道");
+				break;
+			case "東京":
+				// prefが"東京"の場合
+				System.out.println("東京都");
+				break;
+			case "京都":
+				// prefが"京都"の場合
+				System.out.println("京都府");
+				break;
+			default:
+				// prefが上記以外の場合
+				System.out.println("当てはまりません。");
+				break;
+		}
+		
 	}
 }
