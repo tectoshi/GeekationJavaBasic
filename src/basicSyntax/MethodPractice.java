fba39c04e97472c76c62c5657f75eed3874b6968
tectoshi <116927616+tectoshi@users.noreply.github.com>
2024-02-14 18:29:41 +0900

    return時のintへの不要なキャストの修正

--- src/basicSyntax/MethodPractice.java
+++ src/basicSyntax/MethodPractice.java
@@ -2,7 +2,7 @@ package basicSyntax;
 
 public class MethodPractice {
 	public static void main (String[] args) {
-		int a = 12;
+		int a = 90;
 		int b = 7;
 		
 		// メソッド1の呼び出し
@@ -18,25 +18,25 @@ public class MethodPractice {
 	// メソッド1: 二つの値を足し算するメソッド
 	public static int addMethod(int a, int b) {
 		int sum = a + b;
-		return (int)sum;
+		return sum;
 	}
 	
 	// メソッド2: 二つの値を引き算するメソッド
 	public static int subtractionMethod(int a, int b) {
 		int sub = a - b;
-		return(int) sub;
+		return sub;
 	}
 
 	// メソッド3: 二つの値を掛け算するメソッド
 	public static int multiplicationMethod(int a, int b) {
 		int mul = a * b;
-		return (int) mul;
+		return mul;
 	}
 	
 	// メソッド4: 二つの値を割り算するメソッド
 	public static int divisionMethod(int a, int b) {
 		int div = a / b;
-		return (int)div;
+		return div;
 	}
 	// メソッド5: コンソールに出力するためのメソッド
 	// 課題では引数intと指定がありましたが、こちらの方が便利・学習になると思い、intの配列を引数に設定しております。
